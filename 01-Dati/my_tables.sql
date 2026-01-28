f9acd2bcb781e79f1ca228ef8f2aee195009a13d
Adalberto Maria Caccia <adacaccia@gmail.com>
2026-01-28 20:30:54 +0100

    Modificata la noce del brasile

--- 01-Dati/my_tables.sql
+++ 01-Dati/my_tables.sql
@@ -17,7 +17,7 @@ INSERT INTO my_diet VALUES('iodine_supplement',1.0,'iodine_supplement','Integraz
 INSERT INTO my_diet VALUES('tap_water',300.0,'tap_water','Pranzo');
 INSERT INTO my_diet VALUES('my_green_tea',250.0,'my_green_tea','Sanck');
 INSERT INTO my_diet VALUES('d3k2_combo',1.0,'d3k2_combo','Integrazione');
-INSERT INTO my_diet VALUES('1023303',5.0,'brazil_nut_1pc','Integrazione');
+INSERT INTO my_diet VALUES('1023303',3.0,'brazil_nut_1pc','Integrazione');
 INSERT INTO my_diet VALUES('my_green_tea',250.0,'my_green_tea','Mattina');
 INSERT INTO my_diet VALUES('tap_water',500.0,'tap_water','Colazione');
 INSERT INTO my_diet VALUES('tap_water',500.0,'tap_water','Mattina');
